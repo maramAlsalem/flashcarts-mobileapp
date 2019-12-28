cd97273cf74d67c9df4ab3341bacce607ab8e2e5
maramAlsalem <52864275+maramAlsalem@users.noreply.github.com>
2019-12-29 02:36:59 +0300

    Update DecksList.js

--- components/DecksList.js
+++ components/DecksList.js
@@ -19,7 +19,7 @@ class DecksList extends Component {
         });
     }
 
-    async componentWillUpdate() {
+    async componentDidUpdate() {
         const decks = await getAllDecks();
 
         this.setState({
